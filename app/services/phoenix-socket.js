8912b4ab53534b7a22a56063f1bce2c64ded37cf
Jon <jonchoukroun@gmail.com>
2018-05-23 17:59:14 -0700

    Extend joinChannel function to allow rejoining with stored hash_id

--- app/services/phoenix-socket.js
+++ app/services/phoenix-socket.js
@@ -5,13 +5,20 @@ import ENV        from '../config/environment'
 
 export default Service.extend({
 
+  gameChannel: null,
+
   connect(params) {
     let name    = params.name;
+    let hash_id = params.hash_id;
     let socket  = this._openSocket(ENV.api_url);
 
     localStorage.setItem('player_name', name);
 
-    return this._joinChannel(socket, name);
+    if (hash_id) {
+      return this._reJoinChannel(socket, name, hash_id);
+    } else {
+      return this._joinChannel(socket, name, hash_id);
+    }
   },
 
   _openSocket(url) {
@@ -21,14 +28,32 @@ export default Service.extend({
   },
 
   _joinChannel(socket, name) {
-    let channel = socket.channel("game:" + name, { screen_name: name });
+    let channel = socket.channel("game:" + name, { player_name: name });
 
     channel.join()
       .receive("ok", res => {
-        this._handleSuccess("Connected successfully", res)
+        localStorage.setItem('player_hash', res.hash_id);
+        this._handleSuccess("Connected successfully", res);
       })
       .receive("error", res => {
         this._handleFailure("Couldn't connect", res);
+      })
+
+    set(this, 'gameChannel', channel);
+  },
+
+  _reJoinChannel(socket, name, hash_id) {
+    let channel = socket.channel("game:" + name, { player_name: name, hash_id: hash_id });
+
+    channel.join()
+      .receive("ok", res => {
+        localStorage.setItem('player_hash', res.hash_id);
+        this._handleSuccess("Reconnected successfully", res)
+      })
+      .receive("error", res => {
+        localStorage.setItem('player_name', null);
+        localStorage.setItem('player_hash', null);
+        this._handleFailure("Couldn't reconnect", res);
       })
 
     set(this, 'gameChannel', channel);
