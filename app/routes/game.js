fd56d5f3e6eab4045246ed808e146643879af2f4
Jon <jonchoukroun@gmail.com>
2018-05-26 16:37:13 -0700

    Redirect to home if no game state exists

---
+++ app/routes/game.js
@@ -0,0 +1,14 @@
+import Route from '@ember/routing/route'
+import { get } from '@ember/object'
+
+export default Route.extend({
+
+  beforeModel() {
+    this._super(...arguments);
+
+    if (!get(this, 'socket.gameStatus')) {
+      this.transitionTo('home');
+    }
+  },
+
+})
