818ffdffa4ad9a634c7cdd3ee513fa6e91650cea
Jon <jonchoukroun@gmail.com>
2018-05-26 16:36:38 -0700

    Hard code station names and display

---
+++ app/fixtures/subway-stations.js
@@ -0,0 +1,9 @@
+const subwayStations = [
+  {name: 'downtown', display: 'Downtown'},
+  {name: 'venice_beach', display: 'Venice Beach'},
+  {name: 'koreatown', display: 'Koreatown'},
+  {name: 'culver_city', display: 'Culver City'},
+  {name: 'silverlake', display: 'Silverlake'}
+];
+
+export { subwayStations }
