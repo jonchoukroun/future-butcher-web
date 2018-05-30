9c0143718940dfec4e3087c14b3c6a68840e86e6
Jon <jonchoukroun@gmail.com>
2018-05-30 15:15:44 -0700

    Add top black border to game options

--- app/components/game-options.js
+++ app/components/game-options.js
@@ -3,7 +3,8 @@ import Component from '@ember/component'
 export default Component.extend({
 
   classNames: [
-    'd-flex', 'w-100', 'mx-auto', 'align-items-center', 'justify-content-around', 'fixed-bottom'
+    'd-flex', 'w-100', 'mx-auto', 'align-items-center', 'justify-content-around', 'fixed-bottom',
+    'border-top', 'border-black'
   ]
 
 
