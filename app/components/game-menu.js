a2076b0fa60311c2bb681e054b8e1d860c6aee20
Jon <jonchoukroun@gmail.com>
2018-05-29 23:12:58 -0700

    Send bound action when socket gameStatus is set

--- app/components/game-menu.js
+++ app/components/game-menu.js
@@ -1,7 +1,13 @@
 import Component from '@ember/component'
-import { get } from '@ember/object'
+import { get, observer } from '@ember/object'
 
 export default Component.extend({
+
+  gameStarted: observer('socket.gameStatus', function() {
+    if (get(this, 'socket.gameStatus')) {
+      get(this, 'sendGameStarted')();
+    }
+  }),
 
   actions: {
 
