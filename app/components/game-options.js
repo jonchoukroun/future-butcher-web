829c4c1be60fb9647d9dc78dd1b4140a186bfec5
Jon <jonchoukroun@gmail.com>
2018-05-30 11:02:46 -0700

    Convert game options from button group to row of secondary buttons

--- app/components/game-options.js
+++ app/components/game-options.js
@@ -3,7 +3,7 @@ import Component from '@ember/component'
 export default Component.extend({
 
   classNames: [
-    'd-flex', 'flex-row', 'align-items-center', 'justify-content-around', 'fixed-bottom'
+    'd-flex', 'w-100', 'mx-auto', 'align-items-center', 'justify-content-around', 'fixed-bottom'
   ]
 
 
