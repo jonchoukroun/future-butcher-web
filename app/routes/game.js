4787fc98c8323985d3d3e8f74f314e1910c5ccdf
Jon <jonchoukroun@gmail.com>
2018-05-29 23:09:16 -0700

    Guard against non started game process; attempt channel join & restore

--- app/routes/game.js
+++ app/routes/game.js
@@ -1,14 +1,31 @@
 import Route from '@ember/routing/route'
-import { get } from '@ember/object'
+import { get, observer } from '@ember/object'
 
 export default Route.extend({
 
   beforeModel() {
     this._super(...arguments);
 
-    if (!get(this, 'socket.gameStatus')) {
-      this.transitionTo('home');
+    if (get(this, 'socket.gameStatus') !== 'in_game') {
+      this._attemptChannelConnection();
     }
   },
+
+  hasFailedRestore: observer('socket.gameRestore', function() {
+    if (get(this, 'socket.gameRestore') === 'failed') {
+      this.replaceWith('home');
+    }
+  }),
+
+  _attemptChannelConnection() {
+    const name    = localStorage.getItem('player_name');
+    const hash_id = localStorage.getItem('player_hash');
+
+    if (!name || !hash_id) {
+      this.transitionTo('home');
+    } else {
+      get(this, 'socket').connect({name: name, hash_id: hash_id});
+    }
+  }
 
 })
