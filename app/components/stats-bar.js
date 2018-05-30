d1d06bdd5eda83c2f553e6db66c6df7398f4ebe9
Jon <jonchoukroun@gmail.com>
2018-05-29 23:47:22 -0700

    Increment expanded status

--- app/components/stats-bar.js
+++ app/components/stats-bar.js
@@ -5,10 +5,21 @@ export default Component.extend({
 
   collapsed: true,
 
+  _incrementExpandedStatus() {
+    status = localStorage.getItem('expanded_inventory');
+    if (parseInt(status) > 0) {
+      console.log('true')
+      localStorage.setItem('expanded_inventory', ++status);
+    } else {
+      localStorage.setItem('expanded_inventory', 1);
+    }
+  },
+
   actions: {
 
     showExpandedStats() {
       set(this, 'collapsed', false);
+      this._incrementExpandedStatus();
     },
 
     showCollapsedStats() {
