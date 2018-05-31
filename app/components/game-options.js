007cf046c97602e46deb1b325ef5cadc04fd88b1
Jon <jonchoukroun@gmail.com>
2018-05-30 19:34:12 -0700

    Style game options to be raised and opaque

--- app/components/game-options.js
+++ app/components/game-options.js
@@ -2,6 +2,8 @@ import Component from '@ember/component'
 
 export default Component.extend({
 
+  elementId: 'game-options',
+
   classNames: [
     'd-flex', 'w-100', 'mx-auto', 'align-items-center', 'justify-content-around', 'fixed-bottom',
     'border-top', 'border-black'
