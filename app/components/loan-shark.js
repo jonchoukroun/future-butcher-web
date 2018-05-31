082be6e2792406cf62ad377bc17a824155948098
Jon <jonchoukroun@gmail.com>
2018-05-31 08:22:42 -0700

    Implement loan shark debt repayment with guarding on funds

---
+++ app/components/loan-shark.js
@@ -0,0 +1,31 @@
+import Component from '@ember/component'
+import { computed, get } from '@ember/object'
+
+export default Component.extend({
+
+  classNames: ['d-flex', 'flex-column', 'align-items-center', 'justify-content-between', 'p-3'],
+
+  playerFunds: computed('socket.stateData.player.funds', function() {
+    return get(this, 'socket.stateData.player.funds');
+  }),
+
+  playerDebt: computed('socket.stateData.player.debt', function() {
+    return get(this, 'socket.stateData.player.debt');
+  }),
+
+  fundsRemaining: computed('playerFunds', 'playerDebt', function() {
+    return (get(this, 'playerFunds') - get(this, 'playerDebt'));
+  }),
+
+  actions: {
+
+    payDebt() {
+      get(this, 'socket').pushCallBack("pay_debt", { amount: get(this, 'playerDebt') });
+      get(this, 'sendToGameRoute')();
+    },
+
+    clickBack() {
+      get(this, 'sendToGameRoute')();
+    }
+  }
+})
