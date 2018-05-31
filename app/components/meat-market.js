40b9c4fcadcdc4981750cc54adc65ea979344e43
Jon <jonchoukroun@gmail.com>
2018-05-31 07:31:08 -0700

    Set buy/sell menu properties on open actions

--- app/components/meat-market.js
+++ app/components/meat-market.js
@@ -11,6 +11,8 @@ export default Component.extend({
 
   showBuyForm:  false,
   showSellForm: false,
+  buyingCut:    false,
+  sellingCut:   false,
 
   marketCuts: computed('gameStatus', 'socket.stateData.station.market', function() {
     if (get(this, 'socket.gameStatus') !== 'in_game') { return; }
@@ -28,6 +30,16 @@ export default Component.extend({
     closeBuyMenu() {
       set(this, 'showBuyForm', false);
       set(this, 'buyingCut', null);
+    },
+
+    openSellMenu(cut) {
+      set(this, 'showSellForm', true);
+      set(this, 'sellingCut', cut);
+    },
+
+    closeSellMenu() {
+      set(this, 'showSellForm', false);
+      set(this, 'sellingCut', null);
     }
   }
 
