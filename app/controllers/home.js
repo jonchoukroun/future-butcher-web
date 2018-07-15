b8a95ba775e13f8a50ea200726d819b5d0f222e9
Jon <jonchoukroun@gmail.com>
2018-07-15 16:06:03 -0700

    Replace gameChannel observer with redirect to game route

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,18 +1,14 @@
 import Controller from '@ember/controller'
-import { get, observer } from '@ember/object'
+import { get } from '@ember/object'
 
 export default Controller.extend({
-
-  gameStarted: observer('socket.gameStatus', function() {
-    if (get(this, 'socket.gameStatus')) {
-      this.transitionToRoute('game');
-    }
-  }),
 
   actions: {
 
     startGame() {
-      get(this, 'socket').pushCallBack("start_game", {});
+      get(this, 'socket').pushCallBack("start_game", {}).then(() => {
+        this.transitionToRoute('game');
+      })
     }
 
   }
