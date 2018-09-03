788312ee381eb2a9509b577a39b503a0fd62149a
Jon <jonchoukroun@gmail.com>
2018-09-02 19:19:52 -0700

    Update stations

--- app/fixtures/subway-stations.js
+++ app/fixtures/subway-stations.js
@@ -1,9 +1,9 @@
 const subwayStations = [
   {name: 'downtown', display: 'Downtown'},
-  {name: 'venice_beach', display: 'Venice Beach'},
-  {name: 'koreatown', display: 'Koreatown'},
-  {name: 'culver_city', display: 'Culver City'},
-  {name: 'silverlake', display: 'Silverlake'}
+  {name: 'hollywood', display: 'Hollywood'},
+  {name: 'beverly_hills', display: 'Beverly Hills'},
+  {name: 'compton', display: 'Compton'},
+  {name: 'venice_beach', display: 'Venice Beach'}
 ];
 
 export { subwayStations }
