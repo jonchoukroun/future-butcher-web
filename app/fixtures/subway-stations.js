bcf90a35f7a40e52a80321df6ec6412cb9de4555
Jon <jonchoukroun@gmail.com>
2018-09-08 14:19:50 -0700

    Show station details and locations in subway menu

--- app/fixtures/subway-stations.js
+++ app/fixtures/subway-stations.js
@@ -1,9 +1,29 @@
 const subwayStations = [
-  {name: 'downtown', display: 'Downtown'},
-  {name: 'hollywood', display: 'Hollywood'},
-  {name: 'beverly_hills', display: 'Beverly Hills'},
-  {name: 'compton', display: 'Compton'},
-  {name: 'venice_beach', display: 'Venice Beach'}
+  {
+    name:    "downtown",
+    display: "Downtown",
+    details: "Lot's of security."
+  },
+  {
+    name:    "hollywood",
+    display: "Hollywood",
+    details: "High crime. Great returns."
+  },
+  {
+    name:    "beverly_hills",
+    display: "Beverly Hills",
+    details: "Lowest crime. Safe and conservative."
+  },
+  {
+    name:    "compton",
+    display: "Compton",
+    details: "Highest crime. Biggest price swings for the toughest hustlers."
+  },
+  {
+    name:    "venice_beach",
+    display: "Venice Beach",
+    details: "Relatively safe."
+  }
 ];
 
 export { subwayStations }
