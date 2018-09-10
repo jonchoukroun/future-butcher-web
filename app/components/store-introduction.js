768da51454546b83b10b3934eabf6e3bab554e51
Jon <jonchoukroun@gmail.com>
2018-09-10 08:22:42 -0700

    Compute store's weapon and pack inventories

--- app/components/store-introduction.js
+++ app/components/store-introduction.js
@@ -7,4 +7,23 @@ export default Component.extend({
     return get(this, 'socket.stateData.rules.turns_left');
   }),
 
+  storeInventory: computed('socket.stateData.station.store', function() {
+    return get(this, 'socket.stateData.station.store');
+  }),
+
+  weaponsAvailable: computed('storeInventory', function() {
+    const inventory = get(this, 'storeInventory');
+    let weapons = new Map();
+    Object.keys(inventory).filter(i => inventory[i].weight).map(w => weapons.set(w, inventory[w]));
+    return weapons;
+  }),
+
+  packsAvailable: computed('storeInventory', function() {
+    const inventory = get(this, 'storeInventory');
+    let packs = new Map();
+    Object.keys(inventory).filter(i => inventory[i].pack_space).map(p =>
+      packs.set(p, inventory[p]));
+    return packs;
+  })
+
 });
