b5d4d185554bce482458ab0fe1a10d8ea4ed072e
Jon <jonchoukroun@gmail.com>
2018-09-12 21:34:31 -0700

    Handle store screen navigation and transaction alert rendering

--- app/components/store/store-interface.js
+++ app/components/store/store-interface.js
@@ -1,7 +1,10 @@
 import Component from '@ember/component';
-import { computed, get } from '@ember/object';
+import { computed, get, set } from '@ember/object';
+import { later } from '@ember/runloop';
 
 export default Component.extend({
+
+  storeScreen: null,
 
   turnsLeft: computed('socket.stateData.rules.turns_left', function() {
     return get(this, 'socket.stateData.rules.turns_left');
@@ -11,20 +14,64 @@ export default Component.extend({
     return get(this, 'socket.stateData.station.store');
   }),
 
-  weaponsAvailable: computed('storeInventory', function() {
+  weaponsInventory: computed('storeInventory', function() {
     const inventory = get(this, 'storeInventory');
-    let weapons = new Map();
-    Object.keys(inventory).filter(i => inventory[i].weight).map(w =>
-      weapons.set(w, inventory[w]));
+    let weapons = new Object();
+    Object.keys(inventory).filter(i => inventory[i].weight).map(w => weapons[w] = inventory[w]);
     return weapons;
   }),
 
-  packsAvailable: computed('storeInventory', function() {
+  packsInventory: computed('storeInventory', function() {
     const inventory = get(this, 'storeInventory');
-    let packs = new Map();
-    Object.keys(inventory).filter(i => inventory[i].pack_space).map(p =>
-      packs.set(p, inventory[p]));
+    let packs = new Object();
+    Object.keys(inventory).filter(i => inventory[i].pack_space).map(p => packs[p] = inventory[p]);
     return packs;
-  })
+  }),
+
+  weaponsInventoryCount: computed('weaponsInventory', function() {
+    return Object.keys(get(this, 'weaponsInventory')).length;
+  }),
+
+  packsInventoryCount: computed('packsInventory', function() {
+    return Object.keys(get(this, 'packsInventory')).length;
+  }),
+
+  formatCurrency(value) {
+    return (value).toLocaleString("en-us",
+      { style: 'currency', currency: 'USD', minimumFractionDigits: 0 });
+  },
+
+  actions: {
+
+    showWeaponsStore() {
+      set(this, 'storeScreen', 'weapons');
+    },
+
+    showPacksStore() {
+      set(this, 'storeScreen', 'packs');
+    },
+
+    confirmTransaction(payload) {
+      set(this, 'storeScreen', null);
+
+      let message;
+      if (payload.price) {
+        const price = this.formatCurrency(payload.price);
+        message = `${payload.item} ${payload.action} for ${price}.`;
+      } else {
+        message = `${payload.item} ${payload.action}.`;
+      }
+
+      set(this, 'transactionAlert', message);
+      later(() => {
+        set(this, 'transactionAlert', null);
+      }, 2200)
+    },
+
+    back() {
+      set(this, 'storeScreen', null);
+    }
+
+  }
 
 });
