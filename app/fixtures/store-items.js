fddc45b1c5d440121974eb4c7d2abd5b486fca2f
Jon <jonchoukroun@gmail.com>
2018-09-12 21:36:07 -0700

    Display items in stores and allow buying, dropping, and replacing

---
+++ app/fixtures/store-items.js
@@ -0,0 +1,58 @@
+const storeItems = {
+  "hedge_clippers": {
+    display: "Hedge clippers",
+    description: "Good balance of fighting and cutting capacity, right here."
+  },
+  "hockey_stick":   {
+    display: "Hockey stick",
+    description: "Great in a fight, but won't cut much."
+  },
+  "brass_knuckles": {
+    display: "Brass knuckles",
+    description: "Personal defense at a budget."
+  },
+  "box_cutter":     {
+    display: "Box cutter",
+    description: "Not great in a fight, but second to none cutting ability."
+  },
+  "machete":        {
+    display: "Machete",
+    description: "A true all-around beat. Worth every dollar."
+  },
+  "mini_fridge":    {
+    display: "Mini fridge bolted to a skateboard",
+    description: "Just what it sounds like.",
+  },
+  "shopping_cart":  {
+    display: "3-wheeled shopping cart",
+    description: "Can carry a lot, but the squeaking draws attention."
+  },
+  "wheelbarrow":    {
+    display: "Rusty wheelbarrow",
+    description: "Don't worry about the rust, she's got great carrying capacity."
+  },
+  "suitcase":       {
+    display: "Suitcase",
+    description: "Perfect if you want to lug around a few more pounds of... stuff."
+  },
+}
+
+const weaponStats = {
+  "hedge_clippers": {
+    weight: 3
+  },
+  "hockey_stick":   {
+    weight: 5
+  },
+  "brass_knuckles": {
+    weight: 1
+  },
+  "box_cutter":     {
+    weight: 1
+  },
+  "machete":        {
+    weight: 2
+  },
+};
+
+export { storeItems, weaponStats };
