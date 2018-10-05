4e94c1cf4cada85db0e3f626d65b21bf58dd18ea
Jon <jonchoukroun@gmail.com>
2018-10-05 01:34:47 -0700

    Typo

--- app/fixtures/subway-stations.js
+++ app/fixtures/subway-stations.js
@@ -4,35 +4,35 @@ const subwayStations = [
     display: "Downtown",
     cuts_list: "Heart, flank, ribs",
     description: "Tight security.",
-    base_crimte_rate: 2,
+    base_crime_rate: 2,
   },
   {
     name:    "hollywood",
     display: "Hollywood",
     cuts_list: "Loin, liver",
     description: "Rough, grimy, seedy.",
-    base_crimte_rate: 4,
+    base_crime_rate: 4,
   },
   {
     name:    "beverly_hills",
     display: "Beverly Hills",
     cuts_list: "Heart, flank",
     description: "Home of the rich & famous. Safest neighborhood.",
-    base_crimte_rate: 1,
+    base_crime_rate: 1,
   },
   {
     name:    "compton",
     display: "Compton",
     cuts_list: "Heart, flank, ribs, loin, liver",
     description: "Anarchy - free subway rides and certain death.",
-    base_crimte_rate: 5,
+    base_crime_rate: 5,
   },
   {
     name:    "venice_beach",
     display: "Venice Beach",
     cuts_list: "Ribs, loin, liver",
     description: "Relatively safe, but the palm trees are dead.",
-    base_crimte_rate: 3,
+    base_crime_rate: 3,
   }
 ];
 
