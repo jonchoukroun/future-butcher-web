2254483b9d86dcc821d6d6af512a76320d21bd48
Jon <jonchoukroun@gmail.com>
2018-10-11 08:16:35 -0700

    Convert to es6 syntax

--- app/components/stats-bar.js
+++ app/components/stats-bar.js
@@ -1,87 +1,103 @@
-import Component from '@ember/component'
-import { computed, get }   from '@ember/object'
-import $ from 'jquery'
+import Component from '@ember/component';
+import { action, computed } from '@ember-decorators/object';
+import { attribute, classNames } from '@ember-decorators/component';
+import { service } from '@ember-decorators/service';
+import $ from 'jquery';
 
-export default Component.extend({
+@classNames('fixed-top', 'row', 'justify-content-center', 'm-0', 'p-0', 'bg-black')
+export default class StatsBarComponent extends Component {
 
-  elementId: 'stats-bar',
+  @attribute id = "stats-bar";
 
-  classNames: ['row', 'justify-content-center', 'm-0', 'p-0', 'bg-black'],
+  @service router;
 
   didInsertElement() {
     this._super(...arguments);
 
-    if (get(this, 'isFirstTurn') && !localStorage.getItem('closed-turns-tutorial')) {
+    const first_turn = this.get('isFirstTurn');
+    const funds      = this.get('playerFunds');
+    const debt       = this.get('playerDebt');
+    const route      = this.get('router.currentRouteName');
+
+    if (first_turn && !localStorage.getItem('closed-turns-tutorial') && route === "subway") {
+      console.log('should see turnsHelp')
       $('#turnsHelp').slideToggle();
     }
 
-    if (get(this, 'isFirstTurn') && !localStorage.getItem('closed-funds-tutorial') &&
-      get(this, 'playerFunds') > 0) {
+    if (first_turn && !localStorage.getItem('closed-funds-tutorial') && funds > 0) {
+      console.log('should see funds help')
       $('#fundsHelp').slideToggle();
     }
 
-    if (get(this, 'isSecondTurn') && !localStorage.getItem('closed-debt-tutorial') &&
-      get(this, 'playerDebt') > 0) {
+    if (this.get('isSecondTurn') && !localStorage.getItem('closed-debt-tutorial') && debt > 0) {
+      console.log('should see debt help');
       $('#debtHelp').slideToggle();
     }
-  },
-
-  currentStation: computed('socket.stateData.station.station_name', function() {
-    return get(this, 'socket.stateData.station.station_name');
-  }),
-
-  playerFunds: computed('socket.stateData.player.funds', function() {
-    return get(this, 'socket.stateData.player.funds');
-  }),
-
-  playerDebt: computed('socket.stateData.player.debt', function() {
-    return get(this, 'socket.stateData.player.debt');
-  }),
-
-  turnsLeft: computed('socket.stateData.rules.turns_left', function() {
-    return get(this, 'socket.stateData.rules.turns_left');
-  }),
-
-  isFirstTurn: computed('turnsLeft', function() {
-    return get(this, 'turnsLeft') === 24;
-  }),
-
-  isSecondTurn: computed('turnsLeft', function() {
-    return get(this, 'turnsLeft') === 23;
-  }),
-
-  endGame(payload) {
-    get(this, 'socket').pushCallBack('end_game', payload).then(() => {
-      get(this, 'sendQuit')();
-    });
-  },
-
-  actions: {
-
-    closeTurnsTutorial() {
-      localStorage.setItem('closed-turns-tutorial', true);
-      $('#turnsHelp').slideToggle();
-    },
-
-    closeFundsTutorial() {
-      localStorage.setItem('closed-funds-tutorial', true);
-      $('#fundsHelp').slideToggle();
-    },
-
-    closeDebtTutorial() {
-      localStorage.setItem('closed-debt-tutorial', true);
-      $('#debtHelp').slideToggle();
-    },
-
-    toggleStatsBar(selector) {
-      $(selector).slideToggle();
-    },
-
-    quitGame() {
-      let payload = { hash_id: localStorage.getItem('player_hash'), score: 0 };
-      this.endGame(payload);
-    }
-
   }
 
-})
+  @computed('socket.stateData.station.station_name')
+  get currentStation() {
+    return this.get('socket.stateData.station.station_name');
+  }
+
+  @computed('socket.stateData.player.funds')
+  get playerFunds() {
+    return this.get('socket.stateData.player.funds');
+  }
+
+  @computed('socket.stateData.player.debt')
+  get playerDebt() {
+    return this.get('socket.stateData.player.debt');
+  }
+
+  @computed('socket.stateData.rules.turns_left')
+  get turnsLeft() {
+    return this.get('socket.stateData.rules.turns_left');
+  }
+
+  @computed('turnsLeft')
+  get isFirstTurn() {
+    return this.get('turnsLeft') === 24;
+  }
+
+  @computed('turnsLeft')
+  get isSecondTurn() {
+    return this.get('turnsLeft') === 23;
+  }
+
+  endGame(payload) {
+    this.get('socket').pushCallBack('end_game', payload).then(() => {
+      this.get('sendQuit')();
+    });
+  }
+
+  @action
+  closeTurnsTutorial() {
+    localStorage.setItem('closed-turns-tutorial', true);
+    $('#turnsHelp').slideToggle();
+  }
+
+  @action
+  closeFundsTutorial() {
+    localStorage.setItem('closed-funds-tutorial', true);
+    $('#fundsHelp').slideToggle();
+  }
+
+  @action
+  closeDebtTutorial() {
+    localStorage.setItem('closed-debt-tutorial', true);
+    $('#debtHelp').slideToggle();
+  }
+
+  @action
+  toggleStatsBar(selector) {
+    $(selector).slideToggle();
+  }
+
+  @action
+  quitGame() {
+    let payload = { hash_id: localStorage.getItem('player_hash'), score: 0 };
+    this.endGame(payload);
+  }
+
+}
