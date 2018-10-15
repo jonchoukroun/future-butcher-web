a2af4c204bc5a129a58029552248ac8d61a29552
Jon <jonchoukroun@gmail.com>
2018-10-15 07:52:55 -0700

    Conver to es6 classes; layout, copy fixes

--- app/routes/traveling.js
+++ app/routes/traveling.js
@@ -1,24 +1,43 @@
-import Route from '@ember/routing/route'
-import { get } from '@ember/object'
-import { later } from '@ember/runloop'
+import Route from '@ember/routing/route';
+import { service } from '@ember-decorators/service';
+import { later } from '@ember/runloop';
 
-export default Route.extend({
+export default class TravelingRoute extends Route {
+
+  @service('notification-service') notifications;
 
   beforeModel() {
     this._super(...arguments);
 
     this.handleAnimation();
-  },
+    this.handleNotifications();
+  }
+
+  handleNotifications() {
+    const current_station = this.get('socket.stateData.station.station_name');
+    const turns_left = this.get('socket.stateData.rules.turns_left');
+
+    let message;
+    if (current_station !== "bell_gardens" && turns_left === 20) {
+      message = "Gus's Army Surplus Store is now open in Bell Gardens.";
+    }
+
+    if (turns_left === 5) {
+      message = "Only 5 hours left!";
+    }
+
+    this.get('notifications').pinNotification(message)
+  }
 
   handleAnimation() {
     later(() => {
       this.handleRouteRedirect(0);
     }, 600);
-  },
+  }
 
   handleRouteRedirect(counter) {
     counter   = counter || 0;
-    let state = get(this, 'socket.gameStatus');
+    let state = this.get('socket.gameStatus');
 
     if (!state) {
       this.handleRouteRedirect(counter + 1)
@@ -31,4 +50,4 @@ export default Route.extend({
     }
   }
 
-})
+}
