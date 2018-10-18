70001489a6fc889f341250adb785008654cdbcb7
Jon <jonchoukroun@gmail.com>
2018-10-17 17:58:35 -0700

    Update notifications service to handle queuing; move surge notifications to travel route

--- app/routes/traveling.js
+++ app/routes/traveling.js
@@ -2,6 +2,9 @@ import Route from '@ember/routing/route';
 import { service } from '@ember-decorators/service';
 import { later } from '@ember/runloop';
 
+import { cutStats } from 'future-butcher-web/fixtures/cut-stats';
+import { surgeMessages } from 'future-butcher-web/fixtures/surge-messages';
+
 export default class TravelingRoute extends Route {
 
   @service('notification-service') notifications;
@@ -10,10 +13,11 @@ export default class TravelingRoute extends Route {
     this._super(...arguments);
 
     this.handleAnimation();
-    this.handleNotifications();
+    this.handleTimingNotifications();
+    this.handleSurgeNotifications();
   }
 
-  handleNotifications() {
+  handleTimingNotifications() {
     const current_station = this.get('socket.stateData.station.station_name');
     const turns_left = this.get('socket.stateData.rules.turns_left');
 
@@ -27,6 +31,33 @@ export default class TravelingRoute extends Route {
     }
 
     this.get('notifications').pinNotification(message)
+  }
+
+  handleSurgeNotifications() {
+    const current_station = this.get('socket.stateData.station.station_name');
+    const flank = this.get('socket.stateData.station.market.flank.price');
+    const heart = this.get('socket.stateData.station.market.heart.price');
+    const ribs  = this.get('socket.stateData.station.market.ribs.price');
+
+    if (current_station !== "beverly_hills") { return; }
+
+    if (flank >= cutStats.flank.surgeMinimum) {
+      const flank_messages = surgeMessages.flankMessages;
+      const message = flank_messages[Math.floor(Math.random() * Math.floor(flank_messages.length))];
+      this.get('notifications').pinNotification(message);
+    }
+
+    if (heart >= cutStats.heart.surgeMinimum) {
+      const heart_messages = surgeMessages.heartMessages;
+      const message = heart_messages[Math.floor(Math.random() * Math.floor(heart_messages.length))];
+      this.get('notifications').pinNotification(message);
+    }
+
+    if (ribs >= cutStats.ribs.surgeMinimum) {
+      const ribs_messages = surgeMessages.ribsMessages;
+      const message = ribs_messages[Math.floor(Math.random() * Math.floor(ribs_messages.length))];
+      this.get('notifications').pinNotification(message);
+    }
   }
 
   handleAnimation() {
