039c40c497f3946853b14cb3bd79f7dc8702d1b1
Jon <jonchoukroun@gmail.com>
2018-11-19 21:03:12 -0800

    Update dynamic messages

--- app/fixtures/mugging-quotes.js
+++ app/fixtures/mugging-quotes.js
@@ -3,8 +3,13 @@ const playerName = localStorage.getItem('player_name');
 const defeatQuotes = [
   "He kicks your ass!",
   "The mugger drops you like day-old shit.",
-  "You started something you couldn't finish. The mugger, though, easily finishes you.",
-  "The mugger likes to kick ass and eat people. Lucky for you he wasn't hungry."
+  "You started something you couldn't finish. The mugger, though, finishes you.",
+  "The mugger likes to kick ass and eat people. Lucky for you he wasn't hungry.",
+  "Maybe fighting isn’t your thing. It’s definitely the mugger’s thing.",
+  "The mugger lays you out cold.",
+  "Stick to hustling, because you don’t know how to fight.",
+  "Nice try, tough guy. You’re no match for the mugger.",
+  "Local street kids eat popcorn and watch you take a beating."
 ];
 
 const defeatedButtonText = [
@@ -13,13 +18,18 @@ const defeatedButtonText = [
   "Get back on the saddle",
   "Get back in the game",
   "Dust yourself off",
+  "Get back into it"
 ];
 
 const victoryQuotes = [
   "You're here to get rich and kick ass - and getting rich can wait.",
   "You drop the mugger like day-old shit.",
   `Nobody fucks with the ${playerName}!`,
-  `You stomp the mugger and shout, "${playerName}, remember the name!"`
+  `You stomp the mugger and shout, "${playerName}, remember the name!"`,
+  "You just taught the mugger a lesson!",
+  "Who’s tougher than you? Not the mugger, who you just wrecked.",
+  "It wasn’t a fair fight. You took out that mugger in no time.",
+  "Damn! The local street kids stare at the beatdown you gave that mugger."
 ]
 
 export { defeatQuotes, defeatedButtonText, victoryQuotes };
--- app/fixtures/surge-messages.js
+++ app/fixtures/surge-messages.js
@@ -1,28 +1,33 @@
 const surgeMessages = {
 
   heartMessages: [
-    "Cops busted a local plastic surgeon selling heart on the black market and you're the only hustler carrying heart.",
-    "A tech trillionaire ordered all the heart in town. Only you can get these rich scumbags the heart they crave."
+    "Cops busted a local plastic surgeon selling heart on the black market and you're the only hustler carrying.",
+    "A tech trillionaire ordered all the heart in town. Only you can get these rich scumbags the heart they crave.",
+    "A Temple of Doom remake has fans scurrying for heart - looks like you’re in luck."
   ],
 
   flankMessages: [
     "The doomsday cult supplying the neighborhood with flank just drank the koolaid! You're the only hustler with flank, looks like you can clean up here.",
-    "A virus has made all flank in the neighborhood inedible. Celebrities are offering everything they have for your flank."
+    "A virus has made all flank in the neighborhood inedible. Celebrities are offering everything they have for your flank.",
+    "The New Vegan Church murdered all flank providers in town - you’re the only one left!"
   ],
 
   ribsMessages: [
-    "A new study shows eating ribs reduces crows feet. Celebrities are scrambling for your ribs.",
-    "Local gangs are at war and stopped supplying ribs. Looks like you're the only hustler who can service this demand!"
+    "A new study shows eating ribs eliminates crows feet. Celebrities are scrambling for your ribs.",
+    "Local gangs are at war and stopped supplying ribs. Looks like you're the only hustler who can handle the demand!",
+    "The annual Boyle Heights BBQ Competition is heating up! They’ll buy all the ribs you have."
   ],
 
   brainsMessages: [
     "There's a zombie LARP event in town. Brains are in high demand.",
-    "Dr. Jake convinces wealthy housewives eating brains improves gut health. Prices skyrocket."
+    "Dr. Jake convinces wealthy housewives eating brains improves gut health. Prices skyrocket.",
+    "Exams are coming up at Kanye West Middle School. Students are scrambling to eat some brains!"
   ],
 
   liverMessages: [
     "It's the Hannibal Lecter Club's annual convention. Liver and Chianti prices are outta control.",
-    "Locally grown onion is tasting pretty good, so farmer's markets are stocking up on liver."
+    "Locally grown onion is tasting pretty good, so farmer's markets are stocking up on liver.",
+    "An ancient recipe is trending hard, and the main ingredient is liver."
   ]
 
 }
