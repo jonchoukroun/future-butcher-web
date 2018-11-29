dd1b658682e39f54636e48587553562cd7187867
Jon <jonchoukroun@gmail.com>
2018-11-28 16:05:03 -0800

    Update meta data

--- app/fixtures/surge-messages.js
+++ app/fixtures/surge-messages.js
@@ -1,20 +1,20 @@
 const surgeMessages = {
 
   heartMessages: [
-    "Cops busted a local plastic surgeon selling heart on the black market and you're the only hustler carrying.",
+    "Cops busted a local plastic surgeon selling heart on the black market and you're the only butcher carrying.",
     "A tech trillionaire ordered all the heart in town. Only you can get these rich scumbags the heart they crave.",
     "A Temple of Doom remake has fans scurrying for heart - looks like you’re in luck."
   ],
 
   flankMessages: [
-    "The doomsday cult supplying the neighborhood with flank just drank the koolaid! You're the only hustler with flank, looks like you can clean up here.",
+    "The doomsday cult supplying the neighborhood with flank just drank the koolaid! You're the only butcher with flank, looks like you can clean up here.",
     "A virus has made all flank in the neighborhood inedible. Celebrities are offering everything they have for your flank.",
     "The New Vegan Church murdered all flank providers in town - you’re the only one left!"
   ],
 
   ribsMessages: [
     "A new study shows eating ribs eliminates crows feet. Celebrities are scrambling for your ribs.",
-    "Local gangs are at war and stopped supplying ribs. Looks like you're the only hustler who can handle the demand!",
+    "Local gangs are at war and stopped supplying ribs. Looks like you're the only butcher who can handle the demand!",
     "The annual Boyle Heights BBQ Competition is heating up! They’ll buy all the ribs you have."
   ],
 
