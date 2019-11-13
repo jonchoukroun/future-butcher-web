090668c9804c65b8bf45916d06457014abf7351d
Jon <jonchoukroun@gmail.com>
2019-11-12 21:04:18 -0800

    Hide urls

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'future-butcher-web',
     environment,
     rootURL: '/',
-    locationType: 'hash',
+    locationType: 'none',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
